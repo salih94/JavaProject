468254b2172f72363bffdced068ffda70126170b
salih <salih@DESKTOP-NISUE9E>
2020-04-10 08:57:39 +0300

    Hibernate OneToOne BiDirectional

---
+++ 2020.04.10.HibernateOneToOneBi/src/com/salih/hibernate/HibernateQuery.java
@@ -0,0 +1,43 @@
+package com.salih.hibernate;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.salih.entity.Ogretmen;
+import com.salih.entity.OgretmenDetay;
+
+
+public class HibernateQuery {
+
+	public static void main(String[] args) {
+		
+		SessionFactory sessionFactory = 
+				new Configuration().configure("hibernate.cfg.xml")
+				.addAnnotatedClass(OgretmenDetay.class)
+				.addAnnotatedClass(Ogretmen.class)
+				.buildSessionFactory();
+		
+				//create session
+				Session session = sessionFactory.getCurrentSession();
+				
+				OgretmenDetay ogrDty = new OgretmenDetay("Java","Football","abc.com","salihAlper");
+				Ogretmen ogr =new Ogretmen(ogrDty, "Salih", "Alper","salihalper19942@gmail.com");
+				
+				session.beginTransaction();
+				
+			//	session.save(ogrDty);
+				session.save(ogr);
+				
+				session.getTransaction().commit();
+				sessionFactory.close();
+				
+				
+				
+				
+				
+	}
+
+}
